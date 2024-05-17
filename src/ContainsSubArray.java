e323f2c1b5377da2ef2c3ca399539e8ae73c5197
pravin-singh-parihar <parihar.pravinsingh772005@gmail.com>
2024-05-17 21:58:30 +0530

    Intial commit

---
+++ src/ContainsSubArray.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsSubArray {
+    public static void main(String[] args) {
+        int[] arr={23,2,4,6,7};
+        int k=6;
+        System.out.print(sumArray(arr,k));
+
+    }
+    public static boolean sumArray(int[] arr, int k){
+        Map<Integer,Integer>map=new HashMap<>();
+//        WHY WE IN THEIS CODE WE ARE USING THE -1 BECUSE THE CODE WILL BE CODE WILL BE START FROM THE
+        map.put(0,-1);
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
+            int rem=sum%k;
+            if (map.containsKey(rem)){
+                if (i-map.get(rem)>=2){
+                    return true;
+                }
+            }
+            else {
+                map.put(rem,i);
+            }
+
+        }
+        return false;
+    }
+}
